95343ee75b1a35e3d9334facb29c68c1e0b72117
Spencer Bravo <spencer@codeshrub.com>
2018-10-12 14:25:35 -0700

    Add position vector declaration for moment calcs

--- loadcalcs.m
+++ loadcalcs.m
@@ -19,6 +19,23 @@ UCAR = [];
 % Push Rod
 PR = [];
 
+
+% each component is the distance between the wheel center and the force
+
+% r(position vector) for Tie Rod
+r_TR = []; 
+% r(position vector) for Lower Control Arm, Front
+r_LCAF = [];
+%r(position vector) for Lower Control Arm, Rear
+r_LCAR = [];
+%r(position vector) for Upper Control Arm, Front
+r_UCAF = [];
+%r(position vector) for Upper Control Arm, Rear
+r_UCAR = [];
+%r(position vector) for Push Rod
+r_PR = [];
+
+
 %test cases
 TR = [1,1,1; 10, 10, 10; 5 89 78]
 LCAF = [2,2,2]
@@ -26,6 +43,13 @@ LCAR = [3,3,3]
 UCAF = [4,4,4]
 UCAR = [5,5,5]
 PR = [6,6,6]
+
+r_TR = []
+r_LCAF = []
+r_LCAR = []
+r_UCAF = []
+r_UCAR = []
+r_PR = []
 %end test cases
 
 % takes multiple measurements into consideration
@@ -48,3 +72,4 @@ norm_PR = PR./norm(PR)
 sig_x = [norm_TR(1), norm_LCAF(1), norm_LCAR(1), norm_UCAF(1), norm_UCAR(1), norm_PR(1)]
 sig_y = [norm_TR(2), norm_LCAF(2), norm_LCAR(2), norm_UCAF(2), norm_UCAR(2), norm_PR(2)]
 sig_z = [norm_TR(3), norm_LCAF(3), norm_LCAR(3), norm_UCAF(3), norm_UCAR(3), norm_PR(3)]
+
