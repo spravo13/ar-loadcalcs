442803178a9ed45078ffa37cc96aae6035d321a0
Spencer Bravo <spencer@codeshrub.com>
2018-10-12 14:44:14 -0700

    Finish incorportation moment calcs

--- loadcalcs.m
+++ loadcalcs.m
@@ -36,30 +36,30 @@ r_UCAR = [];
 r_PR = [];
 
 
-%test cases
-TR = [1,1,1; 10, 10, 10; 5 89 78]
-LCAF = [2,2,2]
-LCAR = [3,3,3]
-UCAF = [4,4,4]
-UCAR = [5,5,5]
-PR = [6,6,6]
+%%%test cases%%%
+TR = [1,1,1; 10, 10, 10; 5 89 78];
+LCAF = [2,2,2];
+LCAR = [3,3,3];
+UCAF = [4,4,4];
+UCAR = [5,5,5];
+PR = [6,6,6];
 
-r_TR = [4 34 5]
-r_LCAF = [53 35 2]
-r_LCAR = [24 24 5]
-r_UCAF = [3 53 3]
-r_UCAR = [24 45 3]
-r_PR = [24 2 4; 25 3 5]
-%end test cases
+r_TR = [4 34 5];
+r_LCAF = [53 35 2];
+r_LCAR = [24 24 5];
+r_UCAF = [3 53 3];
+r_UCAR = [24 45 3];
+r_PR = [24 2 4; 25 3 5];
+%%%end test cases%%%
 
 % takes multiple measurements into consideration
 % see ./reduction.m
-[TR] = reduction(TR)
-[LCAF] = reduction(LCAF)
-[LCAR] = reduction(LCAR)
-[UCAF] = reduction(UCAF)
-[UCAR] = reduction(UCAR)
-[PR] = reduction(PR)
+[TR] = reduction(TR);
+[LCAF] = reduction(LCAF);
+[LCAR] = reduction(LCAR);
+[UCAF] = reduction(UCAF);
+[UCAR] = reduction(UCAR);
+[PR] = reduction(PR);
 
 % normalizes (makes into unit vectors). Program assumes magnitude ~= 0
 norm_TR = TR./norm(TR)
@@ -70,16 +70,27 @@ norm_UCAR = UCAR./norm(UCAR)
 norm_PR = PR./norm(PR)
 
 % Checks for r vectors not 1x3. (makes nx3 => 1x3)
-[r_TR] = r_avg(r_TR)
-[r_LCAF] = r_avg(r_LCAF)
-[r_LCAR] = r_avg(r_LCAR)
-[r_UCAF] = r_avg(r_UCAF)
-[r_UCAR] = r_avg(r_UCAR)
-[r_PR] = r_avg(r_PR)
+% see ./r_avg
+[r_TR] = r_avg(r_TR);
+[r_LCAF] = r_avg(r_LCAF);
+[r_LCAR] = r_avg(r_LCAR);
+[r_UCAF] = r_avg(r_UCAF);
+[r_UCAR] = r_avg(r_UCAR);
+[r_PR] = r_avg(r_PR);
 
+% creates components to be used in moment summations
+m_TR = cross(r_TR, norm_TR);
+m_LCAF = cross(r_LCAF, norm_LCAF);
+m_LCAR = cross(r_LCAR, norm_LCAR);
+m_UCAF = cross(r_UCAF, norm_UCAF);
+m_UCAR = cross(r_UCAR, norm_UCAR);
+m_PR = cross(r_PR, norm_PR);
 
+% forms rows of matrix A (forces)
+sig_fx = [norm_TR(1), norm_LCAF(1), norm_LCAR(1), norm_UCAF(1), norm_UCAR(1), norm_PR(1)]
+sig_fy = [norm_TR(2), norm_LCAF(2), norm_LCAR(2), norm_UCAF(2), norm_UCAR(2), norm_PR(2)]
+sig_fz = [norm_TR(3), norm_LCAF(3), norm_LCAR(3), norm_UCAF(3), norm_UCAR(3), norm_PR(3)]
 
-sig_x = [norm_TR(1), norm_LCAF(1), norm_LCAR(1), norm_UCAF(1), norm_UCAR(1), norm_PR(1)]
-sig_y = [norm_TR(2), norm_LCAF(2), norm_LCAR(2), norm_UCAF(2), norm_UCAR(2), norm_PR(2)]
-sig_z = [norm_TR(3), norm_LCAF(3), norm_LCAR(3), norm_UCAF(3), norm_UCAR(3), norm_PR(3)]
-
+sig_mx = [m_TR(1), m_LCAF(1), m_LCAR(1), m_UCAF(1), m_UCAR(1), m_PR(1)]
+sig_my = [m_TR(2), m_LCAF(2), m_LCAR(2), m_UCAF(2), m_UCAR(2), m_PR(2)]
+sig_mz = [m_TR(3), m_LCAF(3), m_LCAR(3), m_UCAF(3), m_UCAR(3), m_PR(3)]
