2952e80df7ae60602c6159d3a481659972007487
Spencer Bravo <spencer@codeshrub.com>
2018-10-12 15:01:53 -0700

    Add rest of program to solve for desired vars

--- loadcalcs.m
+++ loadcalcs.m
@@ -20,7 +20,8 @@ UCAR = [];
 PR = [];
 
 
-% each component is the distance between the wheel center and the force
+% each component is the distance between the wheel center and the force vector
+%multiple measurments are allowed
 
 % r(position vector) for Tie Rod
 r_TR = []; 
@@ -34,6 +35,15 @@ r_UCAF = [];
 r_UCAR = [];
 %r(position vector) for Push Rod
 r_PR = [];
+
+
+% B contains the values of the summation of fx, mx, etc.
+B = [ 		;		%Fx (sum)
+		;		%Fy (sum)
+		;		%Fz (sum)
+		;		%Mx (sum)
+		;		%My (sum)
+		;];		%Mx (sum)
 
 
 %%%test cases%%%
@@ -50,7 +60,10 @@ r_LCAR = [24 24 5];
 r_UCAF = [3 53 3];
 r_UCAR = [24 45 3];
 r_PR = [24 2 4; 25 3 5];
+
+B = [56;89;74;75;689;9];
 %%%end test cases%%%
+
 
 % takes multiple measurements into consideration
 % see ./reduction.m
@@ -61,13 +74,15 @@ r_PR = [24 2 4; 25 3 5];
 [UCAR] = reduction(UCAR);
 [PR] = reduction(PR);
 
+
 % normalizes (makes into unit vectors). Program assumes magnitude ~= 0
-norm_TR = TR./norm(TR)
-norm_LCAF = LCAF./norm(LCAF)
-norm_LCAR = LCAR./norm(LCAR)
-norm_UCAF = UCAF./norm(UCAF)
-norm_UCAR = UCAR./norm(UCAR)
-norm_PR = PR./norm(PR)
+norm_TR = TR./norm(TR);
+norm_LCAF = LCAF./norm(LCAF);
+norm_LCAR = LCAR./norm(LCAR);
+norm_UCAF = UCAF./norm(UCAF);
+norm_UCAR = UCAR./norm(UCAR);
+norm_PR = PR./norm(PR);
+
 
 % Checks for r vectors not 1x3. (makes nx3 => 1x3)
 % see ./r_avg
@@ -78,6 +93,7 @@ norm_PR = PR./norm(PR)
 [r_UCAR] = r_avg(r_UCAR);
 [r_PR] = r_avg(r_PR);
 
+
 % creates components to be used in moment summations
 m_TR = cross(r_TR, norm_TR);
 m_LCAF = cross(r_LCAF, norm_LCAF);
@@ -86,11 +102,18 @@ m_UCAF = cross(r_UCAF, norm_UCAF);
 m_UCAR = cross(r_UCAR, norm_UCAR);
 m_PR = cross(r_PR, norm_PR);
 
-% forms rows of matrix A (forces)
-sig_fx = [norm_TR(1), norm_LCAF(1), norm_LCAR(1), norm_UCAF(1), norm_UCAR(1), norm_PR(1)]
-sig_fy = [norm_TR(2), norm_LCAF(2), norm_LCAR(2), norm_UCAF(2), norm_UCAR(2), norm_PR(2)]
-sig_fz = [norm_TR(3), norm_LCAF(3), norm_LCAR(3), norm_UCAF(3), norm_UCAR(3), norm_PR(3)]
 
-sig_mx = [m_TR(1), m_LCAF(1), m_LCAR(1), m_UCAF(1), m_UCAR(1), m_PR(1)]
-sig_my = [m_TR(2), m_LCAF(2), m_LCAR(2), m_UCAF(2), m_UCAR(2), m_PR(2)]
-sig_mz = [m_TR(3), m_LCAF(3), m_LCAR(3), m_UCAF(3), m_UCAR(3), m_PR(3)]
+% forms rows of matrix A (forces)
+sig_fx = [norm_TR(1), norm_LCAF(1), norm_LCAR(1), norm_UCAF(1), norm_UCAR(1), norm_PR(1)];
+sig_fy = [norm_TR(2), norm_LCAF(2), norm_LCAR(2), norm_UCAF(2), norm_UCAR(2), norm_PR(2)];
+sig_fz = [norm_TR(3), norm_LCAF(3), norm_LCAR(3), norm_UCAF(3), norm_UCAR(3), norm_PR(3)];
+
+
+% forms rows of matrix A (moments)
+sig_mx = [m_TR(1), m_LCAF(1), m_LCAR(1), m_UCAF(1), m_UCAR(1), m_PR(1)];
+sig_my = [m_TR(2), m_LCAF(2), m_LCAR(2), m_UCAF(2), m_UCAR(2), m_PR(2)];
+sig_mz = [m_TR(3), m_LCAF(3), m_LCAR(3), m_UCAF(3), m_UCAR(3), m_PR(3)];
+
+A = [sig_fx; sig_fy; sig_fz; sig_mx; sig_my; sig_mz];
+
+x = A\B
