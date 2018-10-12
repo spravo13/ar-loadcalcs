892a0a1c5dbef93c4b2c81aae2cdce29851f4594
Spencer Bravo <spencer@codeshrub.com>
2018-10-12 13:01:58 -0700

    Include normalization

--- loadcalcs.m
+++ loadcalcs.m
@@ -19,15 +19,15 @@ UCAR = [];
 % Push Rod
 PR = [];
 
-%TR = [1,1,1; 10, 10, 10]
-%LCAF = [2,2,2]
-%LCAR = [3,3,3]
-%UCAF = [4,4,4]
-%UCAR = [5,5,5]
-%PR = [6,6,6]
+TR = [1,1,1; 10, 10, 10; 5 89 78]
+LCAF = [2,2,2]
+LCAR = [3,3,3]
+UCAF = [4,4,4]
+UCAR = [5,5,5]
+PR = [6,6,6]
 
 % takes multiple measurements into consideration
-
+% see ./reduction.m
 [TR] = reduction(TR)
 [LCAF] = reduction(LCAF)
 [LCAR] = reduction(LCAR)
@@ -35,3 +35,10 @@ PR = [];
 [UCAR] = reduction(UCAR)
 [PR] = reduction(PR)
 
+% normalizes (makes into unit vectors). Program assumes magnitude ~= 0
+norm_TR = TR./norm(TR)
+norm_LCAF = LCAF./norm(LCAF)
+norm_LCAR = LCAR./norm(LCAR)
+norm_UCAF = UCAF./norm(UCAF)
+norm_UCAR = UCAR./norm(UCAR)
+norm_PR = PR./norm(PR)
