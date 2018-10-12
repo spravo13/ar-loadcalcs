e23d212916b4996b4e97c2a9202c7ff63e5311af
Spencer Bravo <spencer@codeshrub.com>
2018-10-12 12:09:06 -0700

    Add basis for specifying the component measurments of the suspension members

--- loadcalcs.m
+++ loadcalcs.m
@@ -1,1 +1,20 @@
 clear all, close all, clc
+
+% components of vectors describing the directions of the suspension members
+% and therefore the direction of the resultant forces
+
+% each component should be an nx3 matrix where n is typically 1, but can be
+% any number if multiple measurments are taken
+
+% Tie Rod
+TR = [];
+% Lower Control Arm, Front
+LCAF = [];
+% Lower Control Arm, Rear
+LCAR = [];
+% Upper Control Arm, Front
+UCAF = [];
+% Upper Control Arm, Rear
+UCAR = [];
+% Push Rod
+PR = [];
